3b37a3301ace4c14d5c23b6bf623011f45a024c7
phillipug <phillipmusiime@gmail.com>
2020-05-05 17:49:45 +0300

    added user instance methods

--- lib/user.rb
+++ lib/user.rb
@@ -1,68 +1,72 @@
 
 
-
+require "nokogiri"
+require "open-uri"
 
 class User
-	include UrlParser
+	# attr_reader :get_name, :get_nickname, :get_work, :get_location, :get_bio, :get_website, :get_pinned_repos
+	URL = "https://github.com/"
 
-	# attr_reader :get_name
-
-	
 	def initialize(username)
 		@username = username
+		@user_url = URL + @username
+		@profile_page = Nokogiri::HTML.parse(URI.open(@user_url))
 	end
 
 	def get_name
-		get_username(@username)
+		name = @profile_page.css('span.p-name')
+		name ? name.text : "![ field blank ]"
 	end
 
 	def get_nickname
-
+		nickname = @profile_page.css('span.p-nickname')
+		nickname ? nickname.text : '![ field blank ]'
 	end
 
 	def get_work
-		
+		work = @profile_page.css('span.p-org')[0]
+		work ? work.text : '![ field blank ]'
 	end
 
 	def get_location
-		
+		location = @profile_page.css('span.p-label')[0]
+		location ? location.text : "![ field blank ]"
 	end
 
-	def get_email
-		
+	def get_website
+		website = @profile_page.css('li.vcard-detail a')[1]
+		website ? website.text : "![ field blank ]"
 	end
 
 	def get_bio
-		
+		bio = @profile_page.css('div.user-profile-bio')[0]
+		bio ? bio.text.gsub(/\n/, '') : "![ field blank ]"
 	end
 
 	def get_pinned_repos
-
+		pinned_repos = @profile_page.css('.pinned-item-list-item')
+		pinned = []
+		pinned_repos.each do |item|
+			pinned << item.css('span.repo').text
+		end
+		pinned
 	end
 
-	def get_repos
+	# def get_repos
 
-	end
+	# end
 
-	def get_projects
+	# def get_stars
 
-	end
+	# end
 
-	def get_packages
+	# def get_followers
 
-	end
+	# end
 
-	def get_stars
+	# def get_following
 
-	end
-
-	def get_followers
-
-	end
-
-	def get_following
-
-	end
+	# end
 end
 
 
@@ -81,3 +85,15 @@ end
 # 	end
 # 	found_indexes
 # end
+
+
+
+user1 = User.new('dennisosido')
+
+p user1.get_name
+p user1.get_nickname
+p user1.get_bio
+p user1.get_work
+p user1.get_location
+p user1.get_website
+p user1.get_pinned_repos
