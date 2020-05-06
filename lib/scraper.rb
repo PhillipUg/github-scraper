953d8d0c01fa9a70b06b99af9fc340540e9c1f16
phillipug <phillipmusiime@gmail.com>
2020-05-06 14:39:53 +0300

    turned 'profile_page' and 'html' into instance variables. To make methods more testable

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,70 +4,61 @@ require "nokogiri"
 require "open-uri"
 
 class Scraper
-	attr_reader :valid
+	attr_reader :valid, :username
 
 	URL = "https://github.com/"
 
 	def initialize(username)
+		@username = username
 		@user = User.new
-		@user.name = username
-		@user_url = URL + username
+		@user_url = URL + @username
 		
 		@valid = true
 		begin
-     		profile_page = Nokogiri::HTML.parse(URI.open(@user_url))
+     		@profile_page = Nokogiri::HTML.parse(URI.open(@user_url))
     	rescue StandardError
       		@valid = false
       		return
     	end
 		
-		get_name(profile_page)
-		get_nickname(profile_page)
-		get_work(profile_page)
-		get_bio(profile_page)
-		get_location(profile_page)
-		get_website(profile_page)
-		get_pinned_repos(profile_page)
-		get_counters(profile_page)
+		get_name
+		get_nickname
+		get_work
+		get_bio
+		get_location
+		get_website
+		get_pinned_repos
+		get_counters
 	end
 
-	def get_name(profile_page)
-		name = profile_page.css('span.p-name')
+	def get_name
+		name = @profile_page.css('span.p-name')
 		name ? @user.name = name.text : @user.name = "![ field blank ]"
 	end
 
-	def get_nickname(profile_page)
-		nickname = profile_page.css('span.p-nickname')
+	def get_nickname
+		nickname = @profile_page.css('span.p-nickname')
 		nickname ? @user.nickname = nickname.text : @user.nickname = '![ field blank ]'
 	end
 
-	def get_work(profile_page)
-		work = profile_page.css('span.p-org')[0]
+	def get_work
+		work = @profile_page.css('span.p-org')[0]
 		work ? @user.work = work.text : @user.work = '![ field blank ]'
 	end
 
-	def get_location(profile_page)
-		location = profile_page.css('span.p-label')[0]
+	def get_location
+		location = @profile_page.css('span.p-label')[0]
 		location ? @user.location = location.text : @user.location = "![ field blank ]"
 	end
 
-	def get_website(profile_page)
-		website = profile_page.css('li.vcard-detail a')[1]
+	def get_website
+		website = @profile_page.css('li.vcard-detail a')[1]
 		website ? @user.website = website.text : @user.website = "![ field blank ]"
 	end
 
-	def get_bio(profile_page)
-		bio = profile_page.css('div.user-profile-bio')[0]
+	def get_bio
+		bio = @profile_page.css('div.user-profile-bio')[0]
 		bio ? @user.bio = bio.text.gsub(/\n/, '') : @user.bio = "![ field blank ]"
-	end
-
-	def get_pinned_repos(profile_page)
-		pinned_repos = profile_page.css('.pinned-item-list-item')
-		pinned = []
-		pinned_repos.each do |item|
-			pinned << item.css('span.repo').text
-		end
-		@user.pinned = pinned
 	end
 
 	def get_profile_info
@@ -80,35 +71,46 @@ class Scraper
 			@user.location,
 			@user.website
 		].each {|item| user_summary << item}
-		
+
 		@user.summary.each {|item| user_summary << item}
+		@user.pinned.each {|item| user_summary << item}
 		
 		user_summary
 	end
 
-	def get_counters(profile_page)
-		nav = profile_page.css('.UnderlineNav-item')
+	def get_pinned_repos
+		pinned_repos = @profile_page.css('.pinned-item-list-item')
+		pinned = []
+		pinned_repos.each do |item|
+			pinned << item.css('span.repo').text
+		end
+		@user.pinned = pinned
+	end
+
+	def get_counters
+		nav = @profile_page.css('.UnderlineNav-item')
 		nav.each do |item|
 			@user.summary << item.css('.Counter').text.gsub(/\n/, '').gsub(' ', '').to_i
 		end
+		@user.summary
 	end
 
 	def get_page(page)
 		page_url = @user_url + '?tab=' + page
-		html = Nokogiri::HTML.parse(URI.open(page_url))
+		@html = Nokogiri::HTML.parse(URI.open(page_url))
 
 		case 
-		when page == 'repositories' then get_repos(html)
-		when page == 'stars' then get_stars(html)
-		when page == 'followers' then get_followers(html)
-		when page == 'following' then get_following(html)
+		when page == 'repositories' then get_repos
+		when page == 'stars' then get_stars
+		when page == 'followers' then get_followers
+		when page == 'following' then get_following
 		else
 			return ['Error!, category invalid']
 		end
 	end
 
-	def get_repos(html)
-		repos = html.css('li.public')
+	def get_repos
+		repos = @html.css('li.public')
 		counter = 1
 		repoz = []
 		repos.each do |repo|
@@ -118,38 +120,38 @@ class Scraper
 		repoz
 	end
 
-def get_stars(html)
-	stars = html.css('div.d-block')
-	counter = 1
-	starz = []
-	stars.each do |star|
-	starz << ("#{counter}. " + star.css('h3').text.gsub(/\n/, '').gsub(' ', ''))
-	counter += 1
+	def get_stars
+		stars = @html.css('div.d-block')
+		counter = 1
+		starz = []
+		stars.each do |star|
+		starz << ("#{counter}. " + star.css('h3').text.gsub(/\n/, '').gsub(' ', ''))
+		counter += 1
+		end
+		starz
 	end
-	starz
-end
 
-def get_followers(html)
-	followers = html.css('div.table-fixed')
-	counter = 1
-	followerz = []
-	followers.each do |follower|
-	followerz << ("#{counter}. " + follower.css('span.f4').text)
-	counter += 1
+	def get_followers
+		followers = @html.css('div.table-fixed')
+		counter = 1
+		followerz = []
+		followers.each do |follower|
+		followerz << ("#{counter}. " + follower.css('span.f4').text)
+		counter += 1
+		end
+		followerz
 	end
-	followerz
-end
 
-def get_following(html)
-	following = html.css('div.table-fixed')
-	counter = 1
-	followingz = []
-	following.each do |user|
-	followingz << ("#{counter}. " + user.css('span.f4').text)
-	counter += 1
+	def get_following
+		following = @html.css('div.table-fixed')
+		counter = 1
+		followingz = []
+		following.each do |user|
+		followingz << ("#{counter}. " + user.css('span.f4').text)
+		counter += 1
+		end
+		followingz
 	end
-	followingz
-end
 
 
 end
